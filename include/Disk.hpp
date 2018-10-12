2fc30351f8b9745b2191090a5353368526db014a
Michael Matheny <Michael Matheny>
2018-10-11 21:02:52 -0400

    adding new files

---
+++ include/Disk.hpp
@@ -0,0 +1,67 @@
+#ifndef __DISK_H__
+#define __DISK_H__
+
+#include "Range.hpp"
+#include "Point.hpp"
+
+namespace pyscan {
+
+    class Disk : public Range<2> {
+    public:
+        Disk(double x_, double y_, double r_)
+                : origin(x_, y_, 1.0), radius(r_) {}
+
+        Disk()
+                : origin(0.0, 0.0, 1.0), radius(0.0) {}
+
+        Disk(const pt2_t &pt1, const pt2_t &pt2, const pt2_t &pt3) {
+            double x1, x2, x3, y1, y2, y3;
+            x1 = pt1(0);
+            y1 = pt1(1);
+            x2 = pt2(0);
+            y2 = pt2(1);
+            x3 = pt3(0);
+            y3 = pt3(1);
+
+            double a11 = x2 - x1;
+            double a12 = y2 - y1;
+            double a21 = x2 - x3;
+            double a22 = y2 - y3;
+            double b1 = (y2 * y2 + x2 * x2 - y1 * y1 - x1 * x1) / 2.0;
+            double b2 = (y2 * y2 + x2 * x2 - y3 * y3 - x3 * x3) / 2.0;
+
+            double detA = util::det2(a11, a12, a21, a22);
+            // inverse of A
+            double ai11 = a22 / detA;
+            double ai12 = -a12 / detA;
+            double ai21 = -a21 / detA;
+            double ai22 = a11 / detA;
+
+            // A^{-1} b = x
+            double a, b;
+            a = ai11 * b1 + ai12 * b2;
+            b = ai21 * b1 + ai22 * b2;
+            origin = pt2_t(a, b, 1.0);
+            radius = sqrt((x1 - a) * (x1 - a) + (y1 - b) * (y1 - b));
+        }
+
+        inline double getRadius() const {
+            return radius;
+        }
+
+        inline pt2_t getOrigin() const {
+            return origin;
+        }
+
+        virtual inline bool contains(const pt2_t &pt) const final {
+            return util::alte(pt.square_dist(origin), radius * radius);
+        }
+
+    private:
+        pt2_t origin;
+        double radius;
+    };
+
+}
+
+#endif
